b7b13cd3e0f6c43e87ba854d33bc806e12837a56
Damien <damien.nicolleau@epitech.eu>
2024-10-09 17:17:46 +0200

    ADD comments to describe cases

--- index.php
+++ index.php
@@ -33,10 +33,6 @@ var_dump($string);
 // -------------------------
 
 // 4 - Tableau multidimensionnel numéroté
-
-
-// -------------------------
-
 // 5 - Tableau multidimensionnel associatif
 
 /*Tableau multidimensionnel numéroté stockant
@@ -45,6 +41,39 @@ $suite = [
     [1, 2, 4, 8, 16],
     [1, 3, 9, 27, 81]
 ];
+
+var_dump($suite);
+
+// output :
+//
+// array(2) {
+//     [0]=>
+//     array(5) {
+//       [0]=>
+//       int(1)
+//       [1]=>
+//       int(2)
+//       [2]=>
+//       int(4)
+//       [3]=>
+//       int(8)
+//       [4]=>
+//       int(16)
+//     }
+//     [1]=>
+//     array(5) {
+//       [0]=>
+//       int(1)
+//       [1]=>
+//       int(3)
+//       [2]=>
+//       int(9)
+//       [3]=>
+//       int(27)
+//       [4]=>
+//       int(81)
+//     }
+//   }
 
 /*Tableau multidimensionnel numéroté stockant
  *des tableaux associatifs et une valeur simple*/
@@ -55,11 +84,66 @@ $utilisateurs = [
     'Florian'
 ];
 
+var_dump($utilisateurs);
+
+// output : 
+//
+// array(4) {
+//     [0]=>
+//     array(2) {
+//       ["nom"]=>
+//       string(8) "Mathilde"
+//       ["mail"]=>
+//       string(14) "math@gmail.com"
+//     }
+//     [1]=>
+//     array(2) {
+//       ["nom"]=>
+//       string(6) "Pierre"
+//       ["mail"]=>
+//       string(23) "pierre.giraud@edhec.com"
+//     }
+//     [2]=>
+//     array(2) {
+//       ["nom"]=>
+//       string(8) "Amandine"
+//       ["mail"]=>
+//       string(14) "amandine@lp.fr"
+//     }
+//     [3]=>
+//     string(7) "Florian"
+//   }
+
 /*Tableau multidimensionnel associatif stockant
  *des tableaux associatifs*/
 $produits = [
     'Livre' => ['poids' => 200, 'quantite' => 10, 'prix' => 15],
     'Stickers' => ['poids' => 10, 'quantite' => 100, 'prix' => 1.5]
 ];
+
+var_dump($produits);
+
+// output : 
+//
+// array(2) {
+//     ["Livre"]=>
+//     array(3) {
+//       ["poids"]=>
+//       int(200)
+//       ["quantite"]=>
+//       int(10)
+//       ["prix"]=>
+//       int(15)
+//     }
+//     ["Stickers"]=>
+//     array(3) {
+//       ["poids"]=>
+//       int(10)
+//       ["quantite"]=>
+//       int(100)
+//       ["prix"]=>
+//       float(1.5)
+//     }
+//   }
 
 ?>
